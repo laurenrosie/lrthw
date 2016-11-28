e6219875aac2bfd2f0aaf313ba5d95d207acd652
Lauren Rosie <laurenrosiewatson@gmail.com>
2016-11-28 15:22:21 +0000

    Chapter 7, Exercise 7

---
+++ chap07/ex7.rb
@@ -0,0 +1,27 @@
+# puts string
+puts "Mary had a little lamb."
+# puts string with an embedded string
+puts "Its fleece was white as #{'snow'}."
+#puts string
+puts "And everywhere that Mary went."
+# puts '.' ten times in a row
+puts "." * 10
+
+# assigns variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#on the same line prints the variables which are all strings, concatenating them into one string
+print end1 + end2 + end3 + end4 + end5 + end6
+# does the same as above, but afterwards returns to a newline
+puts end7 + end8 + end9 + end10 + end11 + end12
