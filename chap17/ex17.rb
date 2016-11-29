70783ad92959d8883e8f13574160f4cb876d216c
Lauren Rosie <laurenrosiewatson@gmail.com>
2016-11-29 14:49:44 +0000

    Chapter 17, Exercise 17 + other files

---
+++ chap17/ex17.rb
@@ -0,0 +1,24 @@
+from_file, to_file = ARGV
+
+#puts "Copying from #{from_file} to #{to_file}"
+
+in_file = open(from_file)
+indata = in_file.read
+#indata = open(from_file).read
+
+#puts "The input file is #{indata.length} bytes long"
+
+#puts "Does the ouput file exist? #{File.exist?(to_file)}"
+#puts "Ready, hit RETURN to continue, CTRL-C to abort."
+#$stdin.gets
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+
+#open(to_file, 'w').write(open(from_file).read)
+
+#puts "Alright, all done."
+
+in_file.close
+out_file.close
